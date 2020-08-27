842f675bd72a5a235b2e4dca5aed8b0ce389b001
zhizhuodexiaobai <1252087260@qq.com>
2020-08-27 23:33:30 +0800

    version-2

--- Selenium/src/test/java/com/leamon/cases/RegisterTest.java
+++ Selenium/src/test/java/com/leamon/cases/RegisterTest.java
@@ -5,5 +5,6 @@ public class RegisterTest {
         System.out.println("新增了注册接口方法");
         System.out.println("测试提交具体文件合并之后是否会覆盖");
         System.out.println("version-2");
+        System.out.println("version-3");
     }
 }
