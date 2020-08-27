efcdd4b35e104887f3099d05a9de2d292ea57126
zhizhuodexiaobai <1252087260@qq.com>
2020-08-27 23:28:38 +0800

    测试提交具体文件的合并方式

--- Selenium/src/test/java/com/leamon/cases/LoginTest.java
+++ Selenium/src/test/java/com/leamon/cases/LoginTest.java
@@ -3,6 +3,7 @@ package com.leamon.cases;
 public class LoginTest {
     public static void main(String[] args) {
         System.out.println("新增了个main方法");
+        System.out.println("测试提交具体文件的合并方式");
     }
 
 }
